f6e1a5aeaa80a1a17568355a19c94d6bc72f652a
Antonio <adipio@kent.edu>
2022-01-23 17:17:09 -0500

    adding replace functionality in C#.
    adding replace functionality  in C#.

---
+++ C# parse/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Parsing in C#
+// Antonio Di Pio 
+// 1/23/2022
+
+using System; 
+
+public class Example
+{
+    public static void Main() {
+        Console.WriteLine("Enter a string to be parsed");
+        String toBeParsed = Console.ReadLine();
+        //String header = "* A Short String. *";
+        //writes the current line terminator 
+        String parsed = toBeParsed.Replace("/"," ");
+        
+        Console.WriteLine(parsed);
+
+    }
+}
